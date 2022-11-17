bf57590dd06aa884225d52c892af36e3eeb878eb
Malik Smith <smithmalik2468@gmail.com>
2022-11-17 06:25:31 +0000

    Sign-In

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -3,39 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
-import SignUp from './signup';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:  <App />,
-  },
-    {
-    path:"/login",
-    element:<div>This is our login page</div>,
-  },
-  {
-    path: "signup",
-    element: <SignUp/>,
-  }
-  // to create a new path add an object like the below one to this Router.
-  // "path" is that page path; ex: tjcp.com/login
-  // "element" is the react component you want to redirect it to
-  // {
-  //   path:"/login",
-  //   element:<div>Hello world!</div>,
-  // }
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
 
