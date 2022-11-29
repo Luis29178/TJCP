3b05e89b4febb3e4db24e59f4b1f4db87093dc64
Luis Bazaldua <Luis29178@gmail.com>
2022-11-29 12:42:02 -0500

    Fix Button jsx

--- my-react-app/src/Components/Button.jsx
+++ my-react-app/src/Components/Button.jsx
@@ -1,43 +1,22 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> EFTmenu
 import React from 'react'
 import './_Button.css'
 
 const Styles =[
-<<<<<<< HEAD
-    "btn--amber--solid",
-    "btn--gray--solid",
-    "btn--green--solid",
-    "btn--red--solid",
-    "btn--amber--outline",
-    "btn--gray--outline",
-    "btn--green--outline",
-    "btn--red--outline",
-=======
     "btn--Log--basic",
     "btn--Menu--outline",
     "btn--Maps--basic",
     "btn--x--basic",
->>>>>>> EFTmenu
 ]
 
 const Sizes = [
     "btn--tiny",
-<<<<<<< HEAD
-=======
     "btn--maps",
->>>>>>> EFTmenu
     "btn--small",
     "btn--medium",
     "btn--menu",
     "btn--big",
     "btn--max",
-<<<<<<< HEAD
-=======
     "btn--x"
->>>>>>> EFTmenu
 ]
 
 
@@ -67,28 +46,14 @@ export const Button = ({
 
     return (
 
-<<<<<<< HEAD
-        <Button 
-        className={`${checkbtnStyle} ${checkbtnSize}`} 
-        onClick = {onClick} 
-        type = {type}>
-
-        {children}
-        </Button>
-=======
         <button 
         className={`btn ${checkbtnStyle} ${checkbtnSize}`} 
         onClick = {onClick} 
         type = {type}>
         {children}
         </button>
->>>>>>> EFTmenu
 
     );
 
 
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> EFTmenu
