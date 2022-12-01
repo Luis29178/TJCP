961fc8b52dbef1684991278622119e71e647e65d
sergio <sergioncp@gmail.com>
2022-12-01 11:52:01 -0500

    login/logout button now works with auth

--- my-react-app/src/Components/HomePage.js
+++ my-react-app/src/Components/HomePage.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useEffect, useState }  from "react";
 import { Button } from "./Button.jsx";
 import './_HomePage.css';
 
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
 
 function HomePage() {
+    const [isSignedIn, setIsSignedIn] = useState(false); // Local signed-in state.
+    const [isAnon, setIsAnon] = useState(false);
+    // Listen to the Firebase Auth state and set the local state.
+    useEffect(() => {
+      const unregisterAuthObserver = firebase.auth().onAuthStateChanged(user => {
+        setIsSignedIn(!!user);
+        var email = user.email;
+        console.log(email === null ? true : false);
+        //setIsAnon()
+        //console.log(isAnon)
+      });
+      return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+    }, []); 
+    if (isSignedIn) {}
     return (
-        
             <div className="hcontainer">
                 
                 <div className="hbtnGroup">
@@ -49,24 +65,25 @@ function HomePage() {
                 </div>
                 
                 <div className="abtngroup">
-                <Button
+
+                { !isSignedIn && <Button
                         onClick={() => {window.location.href ="/login"}}
                         type="button"
                         buttonStyle="btn--Log--basic"
                         size="btn--menu"
                     >
                         LogIn
-                    </Button>
+                    </Button>}
                     
                         <div style={{float: "right", marginLeft: 10}}>
-                    <Button
-                        onClick={() => { window.location.href ="/signup" }}
+                    { isSignedIn && <Button
+                        onClick={() => {firebase.auth().signOut()}}
                         type="button"
                         buttonStyle="btn--Log--basic"
                         size="btn--menu"
                     >
                         LogOut
-                    </Button>
+                    </Button>}
                     </div>
                 </div>
                 
