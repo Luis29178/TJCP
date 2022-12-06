20943c30848fe4c0db84cea9dc90645dd623f2f9
Malik Smith <smithmalik2468@gmail.com>
2022-12-06 20:48:44 +0000

    drag-and-drop implemented in Raid

--- my-react-app/src/Components/RaidPage.jsx
+++ my-react-app/src/Components/RaidPage.jsx
@@ -5,9 +5,35 @@ import { Tags } from "./RaidTags.jsx"
 import UserInfoBar from "./UserInfoBar"
 import { UserWheel } from "./UserWheel.jsx"
 import { Grid } from "@mui/material";
-
+import {useSpring, animated} from 'react-spring'
+import {useDrag} from 'react-use-gesture'
 
 function Raid() {
+
+    const wheelPos1 = useSpring({x: 0, y: 0});
+    const bindWheelPos1 = useDrag((params) => {
+        wheelPos1.x.set(params.offset[0]);
+        wheelPos1.y.set(params.offset[1]);
+    })
+
+    const wheelPos2 = useSpring({x: 0, y: 0});
+    const bindWheelPos2 = useDrag((params) => {
+        wheelPos2.x.set(params.offset[0]);
+        wheelPos2.y.set(params.offset[1]);
+    })
+
+    const wheelPos3 = useSpring({x: 0, y: 0});
+    const bindWheelPos3 = useDrag((params) => {
+        wheelPos3.x.set(params.offset[0]);
+        wheelPos3.y.set(params.offset[1]);
+    })
+
+    const wheelPos4 = useSpring({x: 0, y: 0});
+    const bindWheelPos4 = useDrag((params) => {
+        wheelPos4.x.set(params.offset[0]);
+        wheelPos4.y.set(params.offset[1]);
+    })
+
     return (
 
         <div className="raidContainer">
@@ -19,7 +45,7 @@ function Raid() {
                 <div className="raidTags--tags">
                 <Tags  style={"raid--tg--basic"}
                     size={"raid--tg-medium"}>
-
+                
                 </Tags>
                 </div>
 
@@ -38,17 +64,22 @@ function Raid() {
                 alignItems="center"
                 >
                     <Grid>
+                        <animated.div {...bindWheelPos1()}style={{x: wheelPos1.x, y: wheelPos1.y}}>
                         <UserWheel onClick={() =>  { console.log("userWheel clicked") }}></UserWheel>
+                        </animated.div>
                     </Grid>
                     <Grid>
+                    <animated.div {...bindWheelPos2()}style={{x: wheelPos2.x, y: wheelPos2.y}}>
                         <UserWheel onClick={() =>  { console.log("userWheel clicked") }}></UserWheel>
-                    </Grid>
+                        </animated.div>                    </Grid>
                     <Grid>
+                    <animated.div {...bindWheelPos3()}style={{x: wheelPos3.x, y: wheelPos3.y}}>
                         <UserWheel onClick={() =>  { console.log("userWheel clicked") }}></UserWheel>
-                    </Grid>
+                        </animated.div>                    </Grid>
                     <Grid>
+                    <animated.div {...bindWheelPos4()}style={{x: wheelPos4.x, y: wheelPos4.y}}>
                         <UserWheel onClick={() =>  { console.log("userWheel clicked") }}></UserWheel>
-                    </Grid>
+                        </animated.div>                    </Grid>
                 </Grid>
 
 
