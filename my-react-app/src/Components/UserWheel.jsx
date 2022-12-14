cf1396369311f4df138442e1fb48db00421f88da
Sergio <sergionpgmail.com>
2022-12-14 18:39:51 -0500

    player info wheel is updated real time with database

--- my-react-app/src/Components/UserWheel.jsx
+++ my-react-app/src/Components/UserWheel.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./_UserWheel.css"
+import {onSnapshot} from 'firebase/firestore';
+import {playerStatusCollection} from '../preferenceHandler'
 
 export const UserWheel = ({
     onClick,
@@ -8,16 +10,56 @@ export const UserWheel = ({
 
 }) => {
 
+    const [playerInfo, setPlayerInfo] = useState([])
+
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(playerStatusCollection, snapshot => {
+            
+            setPlayerInfo(snapshot.docs.map(doc => ({id:doc.id, data:doc.data()})))
+        
+        });
+        return () => {
+            unsubscribe()
+        }
+    },[])
+
+    function parseStatus(statusInfo){
+        let status = "lowStatus";
+
+        switch(statusInfo){
+            case "1":
+                status = "outStatus"
+                break;
+            case "2":
+                status = "lowStatus"
+                break;
+            case "3":
+                status = "fullStatus"
+                break;
+        }
+
+        return status;
+    }
+
 
     return (
         <button onClick={onClick}>
             <div className="ContainerBorder">
                 <div className="Container">
                     <div className="CenterDot"></div>
-                    <div className="Armor"></div>
-                    <div className="Health"></div>
                     <div className="Sectioner"></div>
-                    <div className="Ammo"></div>
+                    {
+                        playerInfo.map(info => (
+                            <div key="status">
+                                <div className={"Health " + parseStatus(info.data.health)}></div>
+                                
+                                <div className={"Ammo " + parseStatus(info.data.ammo)}></div>
+                                <div className={"Armor " + parseStatus(info.data.armor)}></div>
+                            </div>
+                        ))
+                    }
+                    
                 </div>
 
             </div>
