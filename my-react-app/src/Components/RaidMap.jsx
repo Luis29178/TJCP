3cfbec97e2ae1c304ff449e1797d5e78717d4c5c
Malik Smith <smithmalik2468@gmail.com>
2023-01-27 01:11:56 +0000

    remove tag functionality

--- my-react-app/src/Components/RaidMap.jsx
+++ my-react-app/src/Components/RaidMap.jsx
@@ -6,46 +6,46 @@ import cursor2 from "./Tags/cursor2.png";
 
 class RaidMap extends React.Component{
 
-    constructor(props){
-        super(props);
-        this.state = {
-            cursor: cursor2,
+    // constructor(props){
+    //     super(props);
+    //     this.state = {
+    //         cursor: cursor2,
 
-        }
+    //     }
 
-        this.changeCursor = this.changeCursor.bind(this);
+    //     this.changeCursor = this.changeCursor.bind(this);
 
-        document.addEventListener('build', function({ detail }) {
-            let changeCursor = cursor;
-            switch(detail.keyP){
-                case "q":   
+    //     document.addEventListener('build', function({ detail }) {
+    //         let changeCursor = cursor;
+    //         switch(detail.keyP){
+    //             case "q":   
                     
-                    changeCursor = this.state.cursor === cursor? "wait" : cursor;
-                    break;
-                case "w":
-                    changeCursor = this.state.cursor === cursor2? "wait" : cursor2;
-                    break;
-            }
+    //                 changeCursor = this.state.cursor === cursor? "wait" : cursor;
+    //                 break;
+    //             case "w":
+    //                 changeCursor = this.state.cursor === cursor2? "wait" : cursor2;
+    //                 break;
+    //         }
 
-            this.setState(
-                {cursor: changeCursor}
-            )
-        }.bind(this));
+    //         this.setState(
+    //             {cursor: changeCursor}
+    //         )
+    //     }.bind(this));
 
 
-    }
+    // }
 
-    changeCursor(key){
+    // changeCursor(key){
         
 
-    }
+    // }
 
     
 
 
     render(){
-
-        return <div className="raidMap" style={{cursor: `url(${this.state.cursor}) 60 60, auto`}}>
+// style={{cursor: `url(${this.state.cursor}) 60 60, auto`}}
+        return <div className="raidMap" >
         <MapCanvas
             width={1000}
             height={500}
